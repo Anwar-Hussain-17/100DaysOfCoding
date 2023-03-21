d9c1f1d1845abef4b65a3ea8a619f8261da1aea5
Anwar-Hussain-17 <128488617+Anwar-Hussain-17@users.noreply.github.com>
2023-03-21 19:18:36 +0530

    Day 1-30

---
+++ ARRAY/SearchInsertPosition.java
@@ -0,0 +1,13 @@
+//https://leetcode.com/problems/search-insert-position/
+
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int count=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]<target){
+                count++;
+            }
+        } 
+        return count;
+    }
+}
