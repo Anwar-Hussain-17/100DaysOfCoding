74901de7eb1bbfe0fa7551a17072cc75699e4ba6
Anwar-Hussain-17 <128488617+Anwar-Hussain-17@users.noreply.github.com>
2023-03-21 21:52:40 +0530

    Day 1-30

---
+++ ARRAY/NumberofZeroFilledSubarrays.java
@@ -0,0 +1,14 @@
+//https://leetcode.com/problems/number-of-zero-filled-subarrays/
+
+class Solution {
+    public long zeroFilledSubarray(int[] nums) {
+       long ans =0;
+       for(int i=0,j=0;i<nums.length;++i){
+           if(nums[i]!=0){
+               j=i+1;
+           }
+           ans+=i-j+1;
+       }
+       return ans;
+    }
+}
